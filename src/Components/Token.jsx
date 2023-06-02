8ac8497df414c75fa1f5883471908e16328e7525
Mikail Rokib <atandarokib@gmail.com>
2023-06-02 15:29:47 +0100

    updated

--- src/Components/Token.jsx
+++ src/Components/Token.jsx
@@ -18,7 +18,7 @@ const Token = () => {
     <section className="py-[50px] w-full bg-pr">
       <div className="container text-center ">
         <h1 className="token-h1 text-5xl md:text-6xl capitalize font-Japanese_3017"></h1>
-        <ul className="flex flex-col gap-3 list-disc list-inside text-xs font-mono ">
+        <ul className="flex flex-col gap-3 list-disc list-inside text-xs font-mono max-w-[600px] mx-auto">
           <li className=" capitalize">100,000,000 china supply</li>
           <li className=" capitalize">
             Deflationary Mechanism: A portion of each transaction will be
@@ -45,7 +45,7 @@ const Token = () => {
             <h2 className="text-4xl font-Audiowide text-primary">Renounced</h2>
           </article>
           <article className="h-[350px] bg-text rounded-lg flex justify-center items-center flex-col">
-            <h2 className="text-4xl font-Audiowide text-primary">and Locked</h2>
+            <h2 className="text-4xl font-Audiowide text-primary"> Locked</h2>
           </article>
         </div>
       </div>
